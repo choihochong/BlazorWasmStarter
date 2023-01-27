370767b14cc9b6672ce76f4a23ec83676e662902
chong <chong@clinic1st.com>
2023-01-27 09:31:01 +0800

    Add more blazorise sample

---
+++ Shared/Models/UIDemo/PersonValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BlazorStarterkitWasm.Shared.Models.UIDemo;
+
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(vm => vm.FirstName)
+            .NotEmpty()
+            .MaximumLength(30);
+
+        RuleFor(vm => vm.LastName)
+            .NotEmpty()
+            .MaximumLength(30);
+
+        RuleFor(vm => vm.Age)
+            .GreaterThanOrEqualTo(18);
+    }
+}
---
+++ Shared/Models/UIDemo/Salary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStarterkitWasm.Shared.Models.UIDemo
+{
+    public class Salary
+    {
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+    }
+}
